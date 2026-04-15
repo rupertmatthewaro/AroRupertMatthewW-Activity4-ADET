0461ea4fa5625d59f0022704e15c7b0b3275c445
rupertmatthewaro <rupertmatthewaro@gmail.com>
2026-04-15 23:45:30 +0800

    Initial commit

---
+++ AroRupertMatthewW_Activity4_ADET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Aro__Rupert_Matthew_W.__Activity_4___ADET_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
